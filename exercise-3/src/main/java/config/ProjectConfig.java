7f6491ec93da68f757738afc4133ec45f0c6515e
German Gambarte <germangambarte@gmail.com>
2025-02-16 20:41:43 -0300

    add: exercise 3

---
+++ exercise-3/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,8 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "main")
+public class ProjectConfig{}
