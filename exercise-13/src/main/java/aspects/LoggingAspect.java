f34d353be7beb913397ea9d9b265082863b7a086
German Gambarte <germangambarte@gmail.com>
2025-02-18 18:04:45 -0300

    change: aspects execution order

--- exercise-13/src/main/java/aspects/LoggingAspect.java
+++ exercise-13/src/main/java/aspects/LoggingAspect.java
@@ -3,11 +3,13 @@ package aspects;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 
 import java.util.logging.Logger;
 
 
 @Aspect
+@Order(2)
 public class LoggingAspect {
     private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
 
